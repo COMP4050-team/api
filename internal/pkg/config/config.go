746a9c17cd37c5d65ada6af68019bf359ab5d0a9
Jordan Bertasso <36979824+jordanbertasso@users.noreply.github.com>
2022-09-06 07:08:44 +0300

    Update default db path (#11)

--- internal/pkg/config/config.go
+++ internal/pkg/config/config.go
@@ -18,7 +18,7 @@ func NewConfig() Config {
 
 	flag.StringVar(&c.JWTSecret, "jwt-secret", os.Getenv("JWT_SECRET"), "The JWT secret to use. Required")
 	flag.IntVar(&c.Port, "port", 8080, "The port to listen on. Default is 8080")
-	flag.StringVar(&c.DBFilePath, "db-path", "test.db", "The path to the sqlite3 database. Default is test.db")
+	flag.StringVar(&c.DBFilePath, "db-path", "db.sqlite", "The path to the sqlite3 database. Default is db.sqlite")
 	flag.StringVar(&c.TestExecutorEndpoint, "test-executor-endpoint", "http://localhost:8080/", "The endpoint to the test executor. Default is http://localhost:8080/")
 
 	flag.Parse()
